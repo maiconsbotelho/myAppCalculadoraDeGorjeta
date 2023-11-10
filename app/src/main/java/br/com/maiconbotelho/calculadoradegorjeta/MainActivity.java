52401034647731d1ce79579016a3143f8332b6e5
Maicon Botelho <maicon@maiconbotelho.com.br>
2023-11-09 21:56:30 -0300

    Primeiro Commit

---
+++ app/src/main/java/br/com/maiconbotelho/calculadoradegorjeta/MainActivity.java
@@ -0,0 +1,70 @@
+package br.com.maiconbotelho.calculadoradegorjeta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editValor;
+    private TextView textPorcentagem, textGorjeta, textTotal;
+    private SeekBar seekBarGorjeta;
+    private double porcentagem = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editValor = findViewById(R.id.editValor);
+        textGorjeta = findViewById(R.id.textGorjeta);
+        textPorcentagem = findViewById(R.id.textPorcentagem);
+        textTotal = findViewById(R.id.textTotal);
+        seekBarGorjeta = findViewById(R.id.seekBarGorjeta);
+
+        //Adicionar listener SeekBar
+        seekBarGorjeta.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                porcentagem = progress;
+                textPorcentagem.setText(Math.round(porcentagem) + " %");
+                calcular();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    public void calcular(){
+
+        String valorRecuperado = editValor.getText().toString();
+        if (valorRecuperado == null || valorRecuperado.equals("")){
+            Toast.makeText(this, "Digite um valor primeiro", Toast.LENGTH_SHORT).show();
+
+        }else {
+            //Converter String para double
+            double valorDigitado = Double.parseDouble(valorRecuperado);
+
+            //Calcular gorjeta
+            double gorjeta = valorDigitado * (porcentagem/100);
+            double total = valorDigitado + gorjeta;
+
+            //Exibir a gorjeta e o Total
+            textGorjeta.setText("R$ " + gorjeta);
+            textTotal.setText("R$ " + total);
+        }
+    }
+}
